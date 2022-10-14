3e5b31df266781480438f9471b7f8a9398bfd766
Finnur Eiriksson <finnur.eiriksson@klappir.com>
2022-10-14 11:45:47 +0000

    Add module name mapper to root jest build config and use that config as base for jest config in libs

--- jest.build.config.js
+++ jest.build.config.js
@@ -1,7 +1,14 @@
+const path = require('path');
+
 module.exports = {
   rootDir: __dirname,
   testPathIgnorePatterns: ['node_modules/'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   projects: ['<rootDir>/libs/lib-a/jest.build.config.js'],
+  moduleNameMapper: {
+    '@test/lib-b': require.resolve(
+      path.join(__dirname, '/dist/libs/lib-b/src')
+    ),
+  },
   testEnvironment: 'node',
 };
--- libs/lib-a/jest.build.config.js
+++ libs/lib-a/jest.build.config.js
@@ -1,10 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { projects, ...base } = require('../../jest.build.config');
+
 module.exports = {
-  rootDir: __dirname,
-  testPathIgnorePatterns: ['node_modules/'],
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
-  testEnvironment: 'node',
+  ...base,
   displayName: 'lib-a',
-  roots: ['<rootDir>/../../dist/libs/lib-a'],
+  roots: ['<rootDir>/dist/libs/lib-a'],
   moduleDirectories: ['node_modules'],
-  modulePaths: ['<rootDir>/../../dist/libs/lib-a'],
+  modulePaths: ['<rootDir>/dist/libs/lib-a'],
 };
